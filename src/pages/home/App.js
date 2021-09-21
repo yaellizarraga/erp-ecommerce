23655e5775366735f09c4bd5a417cd201344027e
yaellizarraga <yaellizarraga@outlook.es>
2021-09-21 00:31:45 -0600

    Initial commit

---
+++ src/pages/home/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Esta es la pagina principal</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
